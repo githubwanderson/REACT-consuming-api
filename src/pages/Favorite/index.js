e40f847c9eb677c8dfe4177895bb61886839bf5a
Wanderson Alves Martins <emailwandersonalves@gmail.com>
2023-03-18 18:47:49 -0300

    aula10 deleted movie on favorites

--- src/pages/Favorite/index.js
+++ src/pages/Favorite/index.js
@@ -15,9 +15,35 @@ function Favorite(){
       
     },[]);    
 
+    function deleteMovie(id){    
+        /**
+         * Metodo antigo
+         * ARRAY.indexof() - localizo um index de um elemento passado o valor dele
+         * ARRAY.splice() - excluir um index dentro do array()
+         */    
+        // let index = movies.indexOf(id);
+        // setMovies(movies.splice(index));
+
+        /**
+         * Metodo new
+         * ARRAY.filter - recebe uma aerow function
+         * logica - retorna todos os elementos que são !== id 
+         */
+        let filterMovies = movies.filter((item)=>{
+            return (item.id !== id)
+        })
+        // let filterMovies = movies.filter((item)=> item.id !== id)
+
+        setMovies(filterMovies);
+        localStorage.setItem('@primeflix',JSON.stringify(filterMovies));
+    }
+
     return(
         <div className="my-favorites">
             <h1>Meus favoritos</h1>
+
+            {movies.length === 0 && <span>Você não tem nenhum filme salvo!</span>}
+
             <ul>
                 {movies.map((movie)=>{
                     return(
@@ -25,7 +51,7 @@ function Favorite(){
                             <span>{movie.title}</span>
                             <div>
                                 <Link to={`/movies/${movie.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={()=> deleteMovie(movie.id)}>Excluir</button>
                             </div>
                         </li>
                     )
